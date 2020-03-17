267852703581b27b40fd32cc546609e81c4d58ea
Andre-MR-Pereira <54562621+Andre-MR-Pereira@users.noreply.github.com>
2020-03-17 23:40:04 +0000

    Test CustomItem

--- src/com/inaiga/rpgplugin/customitems/CustomItems.java
+++ src/com/inaiga/rpgplugin/customitems/CustomItems.java
@@ -2,12 +2,40 @@ package com.inaiga.rpgplugin.customitems;
 
 import com.inaiga.rpgplugin.menus.MenuManager;
 import com.inaiga.rpgplugin.menus.MenuType;
+import com.inaiga.rpgplugin.player.PlayerManager;
+import com.inaiga.rpgplugin.player.RPGPlayer;
 import org.bukkit.Material;
+import org.bukkit.block.data.type.Bamboo;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
 public enum CustomItems implements CustomItem {
+    STARTER_STAFF_ITEM("Starter Staff", Material.BAMBOO){
+        @Override
+        public void onHit(PlayerInteractEvent event){
+            RPGPlayer Rpgplayer= PlayerManager.getRPGPlayerFromPlayer(event.getPlayer());
+            switch(Rpgplayer.getActiveRPGCharacter().getCharacterClass()){
+                case WIZARD:
+                    event.getPlayer().damage(50);
+                    break;
+                case ROGUE:
+                    event.getPlayer().damage(3);
+                    break;
+                default:
+                    event.getPlayer().damage(9);
+                    break;
+            }
+        }
+
+        @Override
+        public void onUse(PlayerInteractEvent event){
+            RPGPlayer Rpgplayer= PlayerManager.getRPGPlayerFromPlayer(event.getPlayer());
+            event.getPlayer().sendMessage("Using"+ Rpgplayer.getActiveRPGCharacter().getClass());
+        }
+    },
     CHARACTER_MENU_ITEM("Menu", Material.COMPASS){
         @Override
         public void onHit(PlayerInteractEvent event) {
